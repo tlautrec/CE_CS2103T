101f9370389f6e279f2141b955c62f5c250bb003
unknown <CS@Mickey.stu.nus.edu.sg>
2015-09-19 15:34:01 +0800

    Modified test case to make hardcoding fail

--- CE2/src/TextBuddyTest.java
+++ CE2/src/TextBuddyTest.java
@@ -99,9 +99,10 @@ public class TextBuddyTest {
 	public void testSearch() {
 		TextBuddy tb = new TextBuddy("test.txt");
 		TextBuddy.executeCommand("add cats are awesome", tb);
+		TextBuddy.executeCommand("add Really!", tb);
 		TextBuddy.executeCommand("add Would you like a cat?", tb);
 		assertEquals(TextBuddy.executeCommand("search cat", tb), 
-				"1. cats are awesome\n" + "2. Would you like a cat?\n");
+				"1. cats are awesome\n" + "3. Would you like a cat?\n");
 		
 		System.out.println("Passed all search cases!");
 	}
